e85a141ea1736f2dca47e46eb60440db8fd5e189
atdd-bdd <37565969+atdd-bdd@users.noreply.github.com>
2025-06-17 20:11:19 -0400

    Add additional keyword (Assemble, Trigger, etc.)

--- Translaste.cs
+++ Translaste.cs
@@ -292,6 +292,14 @@ namespace GherkinExecutorForCSharp
                 case "Assert":
                 case "Rule":
                 case "Calculation":
+                case "Trigger":
+                case "Verify":
+                case "Assemble":
+                case "Activate":
+                case "Preconditions":
+                case "MainCourse":
+                case "Exception":
+                case "Postconditions":
                     if (pass != 3)
                         break;
                     if (skipSteps)
